47b8cf8108bd808336dcb41516e938dc8d1992a3
alewdy <you@example.com>
2018-07-05 23:58:50 -0400

    train

---
+++ javascript/app.js
@@ -0,0 +1,80 @@
+$(document).ready(function () {
+
+  var config = {
+    apiKey: "AIzaSyA4CbRsazJWgTaeUzJRyDEg8Zy3zt411vc",
+    authDomain: "train-hw-e9d51.firebaseapp.com",
+    databaseURL: "https://train-hw-e9d51.firebaseio.com",
+    projectId: "train-hw-e9d51",
+    storageBucket: "train-hw-e9d51.appspot.com",
+    messagingSenderId: "883392261993"
+  };
+  
+firebase.initializeApp(config);
+//creating my variable to refrence my database
+var dataRef = firebase.database();
+//creating a onclick function for our submit
+$("#submit-btn").on("click", function (event) {
+    event.preventDefault();
+    //Our variables that refrence our users input
+    var trainName = $("#name").val().trim();
+    var destination = $("#destination").val().trim();
+    var timeStart = $("#start-date").val().trim();
+    var frequency = $("#rate").val().trim();
+    // coversions
+    var minutesFormat = moment(timeStart, "hh:mm").subtract(1, "years");
+    currentTime = moment();
+    var timeToarrival = moment().diff(moment(minutesFormat), "minutes");
+    var tRemainder = timeToarrival % frequency;
+    var minutesAway = frequency - tRemainder;
+    // Next Train
+    var nextTrain = moment().add(minutesAway, "minutes");
+    var nextTrainFormat = moment(nextTrain).format("hh:mm");
+    //creating tables variables to then refrence them after and append
+    var rowAdd = $('<tr>');
+    var namehead = $('<td>');
+    namehead.html(trainName);
+
+    var destinationData = $('<td>');
+    destinationData.html(destination);
+
+    var timeData = $('<td>');
+    timeData.html(frequency);
+
+
+    var frequencyData = $('<td>');
+    frequencyData.html(nextTrainFormat);
+
+    var MinutesAwayData = $('<td>');
+    MinutesAwayData.html(minutesAway);
+
+
+    $('tbody').append(rowAdd);
+
+    dataRef.ref().push({
+        name: trainName,
+        dest: destination,
+        time: nextTrainFormat,
+        frequency: frequency,
+        ETA: minutesAway,
+
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+        
+    });
+
+});
+dataRef.ref().on("child_added", function (snapshot) {
+    var newPost = snapshot.val();
+
+    console.log("Name: " + newPost.name);
+    console.log("destination: " + newPost.dest);
+    console.log("time: " + newPost.time);
+    console.log("frequency:" + newPost.freq);
+    console.log("Minutes Away:" + newPost.ETA);
+
+    // append the values stored in database to the table body
+    $(".table > tbody").append("<tr><td>" + newPost.name + "</td><td>" + newPost.dest + "</td><td>" + newPost.frequency + "</td><td>" + newPost.time + "</td><td>" + newPost.ETA + "</td></tr>");
+});
+
+
+});
+
